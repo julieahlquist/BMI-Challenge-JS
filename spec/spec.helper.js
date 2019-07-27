6a83766457e2b157f01b4c1ea8e072115295b912
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-27 16:10:18 +0200

    adds tests for message and refractors imp code

--- spec/spec.helper.js
+++ spec/spec.helper.js
@@ -3,3 +3,4 @@ const BrowserHelpers = require('e2e_training_wheels')
 global.browser = new BrowserHelpers()
 global.expect = chai.expect;
 global.Person = require('../src/person')
+global.BMICalculator = require('../src/bmi_calculator')
