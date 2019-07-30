f82e6fbd4c173a181ccdcf729518c700814c1180
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-30 19:51:55 +0200

    changes to obj in all files and fixes src in html

--- src/bmi_calculator.js
+++ src/bmi_calculator.js
@@ -1,52 +1,41 @@
 function BMICalculator() {
 
-    this.metric_bmi = function (object) {
-        let weight = object.weight;
-        let height = object.height;
+    this.metric_bmi = function (obj) {
+        let weight = obj.weight;
+        let height = obj.height;
         if (weight > 0 && height > 0) {
             let finalBmi = weight / (height / 100 * height / 100);
-            object.bmiValue = parseFloat(finalBmi.toFixed(2));
-            this.setBMIMessage(object)
+            obj.bmiValue = parseFloat(finalBmi.toFixed(2));
+            this.setBMIMessage(obj)
         }
     }
 
-    BMICalculator.prototype.imperial_bmi = function(object) {
-        let weight = object.weight;
-        let height = object.height;
+    BMICalculator.prototype.imperial_bmi = function(obj) {
+        let weight = obj.weight;
+        let height = obj.height;
         if (weight > 0 && height > 0) {
           let finalBmi = weight / (height * height) * 703;
-              object.bmiValue = parseFloat(finalBmi.toFixed(2));
-              setBMIMessage(object);
+              obj.bmiValue = parseFloat(finalBmi.toFixed(2));
+              setBMIMessage(obj);
         }
     }
   
-    this.setBMIMessage = function(object) {
-        if (object.bmiValue < 18.5) {
-            object.bmiMessage = "Underweight"
+    this.setBMIMessage = function(obj) {
+        if (obj.bmiValue < 18.5) {
+            obj.bmiMessage = 'Underweight'
         }
-        if (object.bmiValue > 18.5 && object.bmiValue < 25) {
-            object.bmiMessage = "Healthy"
+        if (obj.bmiValue > 18.5 && obj.bmiValue < 25) {
+            obj.bmiMessage = 'Healthy'
         }
-        if (object.bmiValue > 25 && object.bmiValue < 30) {
-            object.bmiMessage = "Overweight"
+        if (obj.bmiValue > 25 && obj.bmiValue < 30) {
+            obj.bmiMessage = 'Overweight'
         }
-        if (object.bmiValue > 30) {
-            object.bmiMessage = "Obese"
+        if (obj.bmiValue > 30) {
+            obj.bmiMessage = 'Obese'
         }
     }
-
-    this.imperial_bmi = function (object) {
-        let weight = object.weight;
-        let height = object.height;
-        if (weight > 0 && height > 0) {
-          let finalBmi = weight / (height * height) * 703;
-              object.bmiValue = parseFloat(finalBmi.toFixed(2));
-              setBMIMessage(object);
-        }
-      };
 }
 
-
-  if (typeof module !== 'undefined' && module.exports) {
+if (typeof module !== 'undefined' && module.exports) {
     module.exports = BMICalculator;
 }
--- src/person.js
+++ src/person.js
@@ -1,6 +1,7 @@
 function Person(attr) {
     this.weight = attr.weight;
     this.height = attr.height;
+    
     this.calculate_bmi = function () {
         calculator = new BMICalculator();
         calculator.metric_bmi(this)
